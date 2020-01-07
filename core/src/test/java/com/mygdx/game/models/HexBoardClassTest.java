06282ebd4ce8396de50d10467737a69e5bf4fb21
ksve <49108390+ksve@users.noreply.github.com>
2020-01-07 03:01:50 -0500

    Update import statements

--- core/src/test/java/com/mygdx/game/models/HexBoardClassTest.java
+++ core/src/test/java/com/mygdx/game/models/HexBoardClassTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Validation tests for the HexBoardClass.
