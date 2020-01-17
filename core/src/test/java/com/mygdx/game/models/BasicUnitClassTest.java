507f625b9af38f148500857e10cb75d755eefb0b
unknown <aaronsven96@gmail.com>
2020-01-16 19:15:05 -0800

    Added String to test

--- core/src/test/java/com/mygdx/game/models/BasicUnitClassTest.java
+++ core/src/test/java/com/mygdx/game/models/BasicUnitClassTest.java
@@ -9,15 +9,17 @@ public class BasicUnitClassTest {
     static BasicUnit soldier, archer;
 
     @BeforeClass
-    public static <T> void UnitSetup(){
-
+    public static <T> void UnitSetup() throws ClassNotFoundException {
+        Class.forName("com.badlogic.gdx.Gdx");
         soldier = BasicUnit.getUnitFromConfig("configuration/soldier.json");
 
 
         //archer = new BasicUnit("archer.json");
     }
+    private String test ="{\"Type\": \"Soldier\",\"Description\": \"An example configuration file for a soldier\",\"Max Health\": 25,\"Strength\": 10,\"Ranged Strength\":" +
+            "0,\"Range\": 1,\"Speed\": 1,\"Area of Effect\": \"Skewer\",\"Attributes\": []}";
 
-    @Test
+@Test
     public void getState(){
         assertEquals("state should be normal", UnitState.normal, soldier.getState());
         assertEquals("state should be normal", UnitState.normal, archer.getState());
