7f2410e4890575f77bed2d8aeab1373c5b66fb76
unknown <aaronsven96@gmail.com>
2020-01-16 19:15:05 -0800

    Added String to test

--- core/src/test/java/com/mygdx/game/models/BasicUnitClassTest.java
+++ core/src/test/java/com/mygdx/game/models/BasicUnitClassTest.java
@@ -8,19 +8,22 @@ import static org.junit.Assert.assertEquals;
 public class BasicUnitClassTest {
     static BasicUnit soldier, archer;
 
-    @BeforeClass
-    public static <T> void UnitSetup(){
+    private static String test ="{\"Type\": \"Soldier\",\"Description\": \"An example configuration file for a soldier\",\"Max Health\": 25,\"Strength\": 10,\"Ranged Strength\":" +
+            "0,\"Range\": 1,\"Speed\": 1,\"Area of Effect\": \"Skewer\",\"Attributes\": []}";
 
-        soldier = BasicUnit.getUnitFromConfig("configuration/soldier.json");
+    @BeforeClass
+    public static void UnitSetup() {
+        soldier = BasicUnit.getUnitFromConfig(test);
 
 
         //archer = new BasicUnit("archer.json");
     }
 
-    @Test
+
+@Test
     public void getState(){
         assertEquals("state should be normal", UnitState.normal, soldier.getState());
-        assertEquals("state should be normal", UnitState.normal, archer.getState());
+        //assertEquals("state should be normal", UnitState.normal, archer.getState());
     }
 
 }
