a7b29fcfbec07c139d6f00c9b08b80d002ac5488
unknown <aaronsven96@gmail.com>
2020-01-18 21:48:44 -0800

    Added actions

---
+++ core/src/com/mygdx/game/models/MoveAction.java
@@ -0,0 +1,23 @@
+package com.mygdx.game.models;
+
+public class MoveAction implements UnitAction {
+    @Override
+    public String getActionName() {
+        return null;
+    }
+
+    @Override
+    public String getActionTexture() {
+        return null;
+    }
+
+    @Override
+    public boolean isValidAction(Position startPosition, Position endPosition, HexMap map) {
+        return false;
+    }
+
+    @Override
+    public boolean applyAction(Position startPosition, Position endPosition, HexMap map) {
+        return false;
+    }
+}
