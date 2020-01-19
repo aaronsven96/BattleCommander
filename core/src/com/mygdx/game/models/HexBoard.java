7a69d814d451766f6fcd6c6a53030d9286e829dc
ksve <49108390+ksve@users.noreply.github.com>
2020-01-19 14:30:03 -0500

    Update comment

--- core/src/com/mygdx/game/models/HexBoard.java
+++ core/src/com/mygdx/game/models/HexBoard.java
@@ -10,6 +10,7 @@ public class HexBoard<T> {
     private T[][] board;
     private int numRows, numColumns;
 
+    // TODO: Use T obj? Remove?
     public HexBoard(T obj, int numRows, int numColumns) {
         if (numRows < 1 || numColumns < 1) throw new IllegalArgumentException("number of rows/columns must be positive");
 
