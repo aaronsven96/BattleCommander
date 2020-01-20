f87511853bf32d2024868c96ff1b43f1f2f559f8
unknown <aaronsven96@gmail.com>
2020-01-19 17:42:49 -0800

    HexMap

--- core/src/com/mygdx/game/models/HexMap.java
+++ core/src/com/mygdx/game/models/HexMap.java
@@ -2,12 +2,22 @@ package com.mygdx.game.models;
 
 import java.util.List;
 
-public interface HexMap {
+public class HexMap  {
+
+    HexBoard<List<Unit>> units;
+
+    HexBoard<Terrain> terrain;
+
+    HexBoard<Boolean> mapShape;
+
+    List<HexBoard<String>> textures;
 
     /**
      * Get the units at a position P
      */
-    List<Unit> getUnits(Position p);
+    List<Unit> getUnits(Position p){
+        return null;
+    }
 
     /**
      *
@@ -18,31 +28,47 @@ public interface HexMap {
     /**
      * Get the terrain at a position P
      */
-    Terrain getTerrain(Position p);
+    Terrain getTerrain(Position p){
+        return null;
+    }
 
     /**
      * Gets all units for a player with id
      */
-    public List<Unit> getUnitsForPlayer(int playerNum);
+    public List<Unit> getUnitsForPlayer(int playerNum){
+        return null;
+    }
 
     /**
      * Returns a List of Strings which refer to the file location of the textures which relate to each layer of the map
      */
-    List<HexBoard<String>> getTextures();
+    List<HexBoard<String>> getTextures(){
+        return null;
+    }
 
     /**
      * Returns a HexMap of Booleans which describes which tiles are in the map
      */
-    HexBoard<Boolean> getMapShape();
+    HexBoard<Boolean> getMapShape(){
+        return null;
+    }
+
+    public static HexMap getHexMapFromConfig(){
+        return null;
+    }
 
     /**
      * Get possible moves
      */
-    List<Position> getPossibleMoves(Action action, int unitId);
+    /*List<Position> getPossibleMoves(Action action, int unitId){
+        return null;
+    }*/
 
     /**
      * returns a list of potentially blocked commands
      */
-    List<Command> getBlockedCommands();
+    /*List<Command> getBlockedCommands(){
+        return null;
+    }*/
 
 }
