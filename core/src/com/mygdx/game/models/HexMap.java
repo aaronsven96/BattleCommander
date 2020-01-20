106c0b1d011554080e0a4c1fc2e29288a1b6dce1
ksve <49108390+ksve@users.noreply.github.com>
2020-01-19 23:41:41 -0500

    Initial commit

--- core/src/com/mygdx/game/models/HexMap.java
+++ core/src/com/mygdx/game/models/HexMap.java
@@ -1,21 +1,49 @@
 package com.mygdx.game.models;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import java.util.List;
 
-public class HexMap  {
-
+public class HexMap {
     HexBoard<List<Unit>> units;
-
     HexBoard<Terrain> terrain;
-
     HexBoard<Boolean> mapShape;
-
     List<HexBoard<String>> textures;
+
+    private HexMap(HexBoard<List<Unit>> units, HexBoard<Terrain> terrain, HexBoard<Boolean> mapShape, List<HexBoard<String>> textures) {
+        this.units = units;
+        this.terrain = terrain;
+        this.mapShape = mapShape;
+        this.textures = textures;
+    }
+
+    // Copy constructor
+    public HexMap(HexMap original) {
+        units = original.units;
+        terrain = original.terrain;
+        mapShape = original.mapShape;
+        this.textures = original.textures;
+    }
+
+    public static HexMap getHexMapFromConfig(String content) {
+        Gson gson = new Gson();
+        JsonObject terrain = gson.fromJson(content, JsonObject.class);
+
+        String type = terrain.get("type").getAsString();
+        int defense = terrain.get("defense").getAsInt();
+        TerrainState terrainState = TerrainState.valueOf(terrain.get("terrainState").getAsString());
+        Texture texture = new Texture(Gdx.files.internal(terrain.get("texture").getAsString()));
+
+        return new Terrain(type, defense, terrainState, texture);
+    }
 
     /**
      * Get the units at a position P
      */
-    List<Unit> getUnits(Position p){
+    List<Unit> getUnits(Position p) {
         return null;
     }
 
@@ -28,32 +56,28 @@ public class HexMap  {
     /**
      * Get the terrain at a position P
      */
-    Terrain getTerrain(Position p){
+    Terrain getTerrain(Position p) {
         return null;
     }
 
     /**
      * Gets all units for a player with id
      */
-    public List<Unit> getUnitsForPlayer(int playerNum){
+    public List<Unit> getUnitsForPlayer(int playerNum) {
         return null;
     }
 
     /**
      * Returns a List of Strings which refer to the file location of the textures which relate to each layer of the map
      */
-    List<HexBoard<String>> getTextures(){
+    List<HexBoard<String>> getTextures() {
         return null;
     }
 
     /**
      * Returns a HexMap of Booleans which describes which tiles are in the map
      */
-    HexBoard<Boolean> getMapShape(){
-        return null;
-    }
-
-    public static HexMap getHexMapFromConfig(){
+    HexBoard<Boolean> getMapShape() {
         return null;
     }
 
