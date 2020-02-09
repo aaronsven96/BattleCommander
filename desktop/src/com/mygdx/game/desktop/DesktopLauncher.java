0f09ffda0923cccd9e0dd826360a0749f329daa1
ksve <49108390+ksve@users.noreply.github.com>
2020-02-08 21:03:30 -0500

    Remove lines

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,8 +3,6 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
-import com.mygdx.game.models.ConfigurationFactory;
-import com.mygdx.game.models.HexMap;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
