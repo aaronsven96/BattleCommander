4982e884396a28541a2369bab88624a15759ce62
Daniel <dannyosven@gmail.com>
2020-02-08 20:17:45 -0500

    Work on ranged attack

--- core/src/com/mygdx/game/models/AttackAction.java
+++ core/src/com/mygdx/game/models/AttackAction.java
@@ -2,23 +2,61 @@ package com.mygdx.game.models;
 
 public class AttackAction implements UnitAction {
 
+    private String name;
+    private String texture;
+    private int distance;
+    private int maxRange;
+    private int damage;
+    private boolean hasSight;
+    boolean isValidAction;
+
+    public AttackAction(String name, String texture, Position startPosition, Position endPosition, int maxRange,
+                        int[] damage) {
+        this.name = name;
+        this.texture = texture;
+        distance = calculateDistance(startPosition, endPosition);
+        this.maxRange = maxRange;
+        this.damage = calculateRangedDamage(distance, damage);
+        hasSight = checkHasSight(startPosition, endPosition);
+    }
+
+    private static int calculateDistance(Position startPosition, Position endPosition) {
+        return 0;
+    }
+
+    private static boolean checkHasSight(Position startPosition, Position endPosition) {
+        return false;
+    }
+
+    private static int calculateRangedDamage(int distance, int[] damage) {
+        if (distance <= damage.length) {
+            return damage[distance-1];
+        } else {
+            return 0;
+        }
+    }
+
     @Override
     public String getActionName() {
-        return null;
+        return name;
     }
 
     @Override
     public String getActionTexture() {
-        return null;
+        return texture;
     }
 
     @Override
-    public boolean isValidAction(Position startPosition, Position endPosition, HexMap map) {
+    public boolean isValidAction() {
+
+        if (damage <= 0){
+            return false;
+        }
         return false;
     }
 
     @Override
-    public boolean applyAction(Position startPosition, Position endPosition, HexMap map) {
+    public boolean applyAction() {
         return false;
     }
 }
