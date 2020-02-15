d81c32d2af625afff463c62ec255eeac5e58615d
ksve <49108390+ksve@users.noreply.github.com>
2020-02-15 08:24:51 -0500

    Update texture field

--- core/src/com/mygdx/game/models/TriggerProximity.java
+++ core/src/com/mygdx/game/models/TriggerProximity.java
@@ -2,6 +2,10 @@ package com.mygdx.game.models;
 
 public class TriggerProximity implements Trigger {
 
+    public TriggerProximity(Unit unit1, Unit unit2, int distance) {
+
+    }
+
     @Override
     public boolean isTriggered(HexMap map) {
         return false;
