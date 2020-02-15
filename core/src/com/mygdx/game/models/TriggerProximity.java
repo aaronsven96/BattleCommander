7f7b88503b9c82de61c848af9606167c4d9ef5ef
ksve <49108390+ksve@users.noreply.github.com>
2020-02-15 11:08:46 -0500

    Update method

--- core/src/com/mygdx/game/models/TriggerProximity.java
+++ core/src/com/mygdx/game/models/TriggerProximity.java
@@ -1,9 +1,42 @@
 package com.mygdx.game.models;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 public class TriggerProximity implements Trigger {
+    String type;
+    int pid1;
+    int pid2;
+    int range;
 
-    public TriggerProximity(Unit unit1, Unit unit2, int distance) {
+    private TriggerProximity(String type, int pid1, int pid2, int range) {
+        this.type = type;
+        this.pid1 = pid1;
+        this.pid1 = pid2;
+        this.range = range;
+    }
 
+    /**
+     * Returns the Trigger from the configuration file.
+     *
+     * @param config the file path
+     * @return the Trigger from the configuration file
+     */
+    public static TriggerProximity getTriggerFromConfig(String config) {
+        Gson gson = new Gson();
+        JsonObject event = gson.fromJson(config, JsonObject.class);
+        JsonArray trigger1 = event.get("events").getAsJsonArray();
+        JsonObject trigger2 = trigger1.get(0).getAsJsonObject();
+        JsonObject trigger3 = trigger2.get("trigger").getAsJsonObject();
+
+        String type = trigger3.get("type").getAsString();
+        int pid1 = trigger3.get("pid1").getAsInt();
+        int pid2 = trigger3.get("pid2").getAsInt();
+        int range = trigger3.get("range").getAsInt();
+
+        return new TriggerProximity(type, pid1, pid2, range);
     }
 
     @Override
