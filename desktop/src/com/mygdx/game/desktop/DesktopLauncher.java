9111afe077fd0e9c5fd2bd85670d90130181fba2
ksve <49108390+ksve@users.noreply.github.com>
2020-03-01 02:36:41 -0500

    Test

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -14,9 +14,9 @@ public class DesktopLauncher {
         //config.width = 1000;
         //config.height = 1000;
         new LwjglApplication(new MyGdxGame(), config);
-//        HexMap map = ConfigurationFactory.getInstance().makeHexMapFromConfig("hex_map.json");
-//        map.save("hex_map.json");
+//        HexMap map = ConfigurationFactory.getInstance().makeHexMapFromConfig("hex_map2.json");
 //        TriggerProximity trigger = ConfigurationFactory.getInstance().makeTriggerFromConfig("configuration/events/events.json");
 //        System.out.println(trigger.isTriggered(map));
+//        map.save("hex_map2.json");
     }
 }
