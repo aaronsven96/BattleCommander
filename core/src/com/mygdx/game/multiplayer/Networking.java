8ed547a99bfb105ef5567c79f10ed97bd04e1c84
chriskhosh <ckhoshatefeh@gmail.com>
2020-03-08 13:59:36 -0400

    connections working

--- core/src/com/mygdx/game/multiplayer/Networking.java
+++ core/src/com/mygdx/game/multiplayer/Networking.java
@@ -22,14 +22,19 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Networking {
+    private static Set<String[]> listenLog = new HashSet<>();
+    private static Set<String[]> serverLog = new HashSet<>();
+    private static ArrayBlockingQueue<String[]> serverQueue = new ArrayBlockingQueue<>(99);
     private static MultiplayerLobby screen;
-    private static Socket serverSocket;
-    private static ServerSocket localSocket;
+    private static Socket serverConnection;
+    private static ServerSocket serverSocket;
     private static String roomOwner;
     private static String roomName;
     private static String roomPassword;
@@ -42,7 +47,8 @@ public class Networking {
     private static Set<String> names = new HashSet<>();
 
     // The set of all the print writers for all the clients, used for broadcast.
-    private static Set<Socket> writers = new HashSet<>();
+//    private static Set<Socket> writers = new HashSet<>();
+    private static HashMap<String, Socket> clientSockets = new HashMap<>();
 
     public String getLocalIp() {
         // The following code loops through the available network interfaces
@@ -82,7 +88,7 @@ public class Networking {
 
     public static AbstractScreen createServer(String[] args) {
         screen = new MultiplayerLobby();
-        createListener();
+        serverListener();
         roomIp = args[0];
         localIp = args[0];
         roomName = args[1];
@@ -97,18 +103,17 @@ public class Networking {
 
     public static AbstractScreen joinServer(String[] args) {
         screen = new MultiplayerLobby();
-        createListener();
         roomIp = args[0];
         localUser = args[1];
         roomPassword = args[2];
         localIp = new Networking().getLocalIp();
-        serverSocket = new Networking().connection(new String[] {roomIp, "joinServer", localUser, roomPassword});
+        clientListener(new String[] {roomIp, "joinServer", localUser, roomPassword});
         screen.addChat("Joined Room IP: " + roomIp);
-        names.add(roomOwner);
+        names.add(localUser);
         return screen;
     }
 
-    public static void createListener() {
+    public static void serverListener() {
         // Now we create a thread that will listen for incoming socket connections
         new Thread(() -> {
             ServerSocketHints serverSocketHint = new ServerSocketHints();
@@ -120,7 +125,7 @@ public class Networking {
             // especially in the lower numbers ( like 21, 80, etc )
             for (int port = 49153; port < 65536; port++) {
                 try {
-                    localSocket = Gdx.net.newServerSocket(Protocol.TCP, port, serverSocketHint);
+                    serverSocket = Gdx.net.newServerSocket(Protocol.TCP, port, serverSocketHint);
                     roomPort = String.valueOf(port);
                     screen.addChat("Room Port: " + roomPort + "\n");
                     break;
@@ -132,14 +137,15 @@ public class Networking {
             // Loop forever
             while(true){
                 // Create a socket
-                Socket socket = localSocket.accept(null);
+                Socket socket = serverSocket.accept(null);
+                clientSockets.put(socket.getRemoteAddress(), socket);
 
                 // Read data from the socket into a BufferedReader
                 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                 try {
                     // Read to the next newline (\n) and display that text on labelMessage
-                    parseData(input.readLine());
+                    parseCommand(new String[] {"server", "in", input.readLine()});
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -147,32 +153,70 @@ public class Networking {
         }).start(); // And, start the thread running
     }
 
-    private Socket connection(String[] data) {
-        SocketHints socketHints = new SocketHints();
-        // Socket will time our in 4 seconds
-        socketHints.connectTimeout = 0;
-        String[] ipInfo = data[0].split(":");
-        data[0] = getLocalIp();
-        String sendData = String.join("<:.:>", data);
-        //create the socket and connect to the server entered in the text box ( x.x.x.x format ) on port
-        Socket socket = Gdx.net.newClientSocket(Protocol.TCP, ipInfo[0], Integer.parseInt(ipInfo[1]), socketHints);
-        try {
-            socket.getOutputStream().write(sendData.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static boolean clientListener(String[] data) {
+        new Thread(() -> {
+            SocketHints socketHints = new SocketHints();
+            // Socket will time our in 4 seconds
+            socketHints.connectTimeout = 0;
+            String[] ipInfo = data[0].split(":");
+            data[0] = new Networking().getLocalIp();
+            String sendData = String.join("<:.:>", data);
+            //create the socket and connect to the server entered in the text box ( x.x.x.x format ) on port
+            serverConnection = Gdx.net.newClientSocket(Protocol.TCP, ipInfo[0], Integer.parseInt(ipInfo[1]), socketHints);
+            try {
+                serverConnection.getOutputStream().write(sendData.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while(true){
+                // Read data from the socket into a BufferedReader
+                BufferedReader input = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
+
+                try {
+                    // Read to the next newline (\n) and display that text on labelMessage
+                    parseCommand(new String[] {"client", "in", input.readLine()});
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        if (serverConnection.isConnected()) {
+            return true;
         }
-        return socket;
+        else {
+            return false;
+        }
     }
 
-    private static void parseData(String input) {
-        String[] inputArray = input.split("<:.:>");
+    private static void parseCommand(String[] command) {
+        listenLog.add(new String[] {String.valueOf(java.time.LocalDateTime.now()), String.valueOf(command)});
+//        String[] inputArray = input.split("<:.:>");
 
-        if (inputArray[1].equals("lobbyMessage")) {
-            screen.addChat(inputArray[1]);
+        if (command[0].equals("server")) {
+            parseServer(command);
         }
-        else if (inputArray[1].equals("joinServer")) {
-            join(inputArray);
+        else if (command[0].equals("client")) {
+            parseClient(command);
         }
+    }
+
+    private static void parseServer(String[] command) {
+        String[] commandArray = command[2].split("<:.:>");
+        System.out.println(command);
+
+        if (commandArray[1].equals("lobbyMessage")) {
+            sendAll(commandArray[2]);
+        }
+        else if (commandArray[1].equals("joinServer")) {
+            join(command);
+        }
+    }
+
+    private static void parseClient(String[] command) {
+
+    }
+
+    private static void sendAll(String s) {
     }
 
     private static void join(String[] inputArray) {
