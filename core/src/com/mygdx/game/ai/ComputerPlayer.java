33a795e4c5c7f169657a5ebddde5e0abba449896
unknown <aaronsven96@gmail.com>
2020-03-12 15:43:02 -0700

    AI start

---
+++ core/src/com/mygdx/game/ai/ComputerPlayer.java
@@ -0,0 +1,4 @@
+package com.mygdx.game.ai;
+
+public class ComputerPlayer {
+}
---
+++ core/src/com/mygdx/game/ai/GroupStrategy.java
@@ -0,0 +1,6 @@
+package com.mygdx.game.ai;
+
+public class GroupStrategy {
+    private int priority;
+    private Strategy strategy;
+}
